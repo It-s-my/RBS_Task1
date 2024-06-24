1f72ea82edeebd3017f4200cca59eb426f2399db
Vadim <96053959+It-s-my@users.noreply.github.com>
2024-06-24 18:25:26 +0300

    fix 1
    Заменил устаревший ionil, добавил flag(usage),исправил нейминг на err, добавил проверку на dir

--- task1.go
+++ task1.go
@@ -10,59 +10,76 @@ import (
 	"strings"
 )
 
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "  %s -src <source_file> -dst <destination_directory>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
 func main() {
-	fileName := flag.String("src", "", "Имя файла")
-	dstPtr := flag.String("dst", "", "Название конечной директории")
+	fileName := flag.String("src", "", "Имя файла")                  //флаг для имени файла
+	dstPtr := flag.String("dst", "", "Название конечной директории") //флаг для названия конечной директории
 	flag.Parse()
 
 	if *fileName == "" {
-		fmt.Println("Необходимо указать имя файла")
+		fmt.Println("Необходимо указать имя файла") //проверка на наличие имени файла
 		return
 	}
 
 	if *dstPtr == "" {
-		fmt.Println("Необходимо указать название конечной директории")
+		fmt.Println("Необходимо указать название конечной директории") //проверка на наличие названия конечной директории
 		return
 	}
 
-	file, read := ioutil.ReadFile(*fileName)
-	if read != nil {
-		fmt.Println("Ошибка чтения файла:", read)
+	file, err := os.ReadFile(*fileName)
+	if err != nil {
+		fmt.Println("Ошибка чтения файла:", err) //проверка чтения файла
 		return
 	}
-	_ = os.MkdirAll(*dstPtr, 0777)
-	content := string(file)
-	lines := strings.Split(content, "\n")
 
-	for _, line := range lines {
+	err = os.MkdirAll(*dstPtr, 0777)
+	if err != nil {
+		fmt.Println("Error creating directory:", err)
+		return
+	}
+
+	fmt.Println("Directory", *dstPtr, "created successfully")
+	//создание директории
+
+	content := string(file)               //получение содержимого файла
+	lines := strings.Split(content, "\n") //разбиение содержимого файла на строки
+
+	for _, line := range lines { //Проверка на корректность ссылок, неправильные сразу отбрасываем
 		u, read := url.Parse(line)
-		if read != nil || u.Scheme == "" || u.Host == "" {
+		if read != nil && u.Scheme == "" && u.Host == "" {
 			continue
 		}
 
-		resp, read := http.Get(line)
+		resp, read := http.Get(line) //Отправляем гет запросы оставшимся ссылкам
 		if read != nil {
 			fmt.Println(line, ":", read)
 			continue
 		}
-		defer resp.Body.Close()
+		defer resp.Body.Close() //закрытие потока
 
-		body, read := ioutil.ReadAll(resp.Body)
+		body, read := ioutil.ReadAll(resp.Body) //чтение ответа
 		if read != nil {
 			fmt.Println(line, ":", read)
 			continue
 		}
-
+		//Сохранение результатов в файлы
 		outputFileName := *dstPtr + "/" + strings.Replace(line, "://", "_", -1) + ".txt"
 		outputFile, create := os.Create(outputFileName)
 		if create != nil {
 			fmt.Println(line, ":", create)
 			continue
 		}
-		defer outputFile.Close()
+		defer outputFile.Close() //закрытие потока
 
 		outputFile.Write(body)
 
-		fmt.Println(line, ":", "Результат сохранен в файл", outputFileName)
+		fmt.Println(line, ":", "Результат сохранен в файл", outputFileName) //Отчет о результате работы
 	}
 }
