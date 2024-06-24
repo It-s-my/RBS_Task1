76f5c48e887271a055314194fe910ed49c31b0f3
Vadim <96053959+It-s-my@users.noreply.github.com>
2024-06-24 16:35:02 +0300

    Ну вроде сделал

--- task1.go
+++ task1.go
@@ -1,62 +1,68 @@
 package main
 
 import (
- "flag"
- "fmt"
- "io/ioutil"
- "net/http"
- "net/url"
- "os"
- "strings"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
 func main() {
- fileName := flag.String("file", "", "имя файла")
- flag.Parse()
+	fileName := flag.String("src", "", "Имя файла")
+	dstPtr := flag.String("dst", "", "Название конечной директории")
+	flag.Parse()
 
- if *fileName == "" {
-  fmt.Println("Необходимо указать имя файла")
-  return
- }
+	if *fileName == "" {
+		fmt.Println("Необходимо указать имя файла")
+		return
+	}
 
- file, read := ioutil.ReadFile(*fileName)
- if read != nil {
-  fmt.Println("Ошибка чтения файла:", read)
-  return
- }
+	if *dstPtr == "" {
+		fmt.Println("Необходимо указать название конечной директории")
+		return
+	}
 
- content := string(file)
- lines := strings.Split(content, "\n")
+	file, read := ioutil.ReadFile(*fileName)
+	if read != nil {
+		fmt.Println("Ошибка чтения файла:", read)
+		return
+	}
+	_ = os.MkdirAll(*dstPtr, 0777)
+	content := string(file)
+	lines := strings.Split(content, "\n")
 
- for _, line := range lines {
-  u, read := url.Parse(line)
-  if read != nil || u.Scheme == "" || u.Host == "" {
-   continue
-  }
+	for _, line := range lines {
+		u, read := url.Parse(line)
+		if read != nil || u.Scheme == "" || u.Host == "" {
+			continue
+		}
 
-  resp, read := http.Get(line)
-  if read != nil {
-   fmt.Println(line, ":", read)
-   continue
-  }
-  defer resp.Body.Close()
+		resp, read := http.Get(line)
+		if read != nil {
+			fmt.Println(line, ":", read)
+			continue
+		}
+		defer resp.Body.Close()
 
-  body, read := ioutil.ReadAll(resp.Body)
-  if read != nil {
-   fmt.Println(line, ":", read)
-   continue
-  }
+		body, read := ioutil.ReadAll(resp.Body)
+		if read != nil {
+			fmt.Println(line, ":", read)
+			continue
+		}
 
-  outputFileName := strings.Replace(line, "://", "_", -1) + ".txt"
-  outputFile, create := os.Create(outputFileName)
-  if create != nil {
-   fmt.Println(line, ":", create)
-   continue
-  }
-  defer outputFile.Close()
+		outputFileName := *dstPtr + "/" + strings.Replace(line, "://", "_", -1) + ".txt"
+		outputFile, create := os.Create(outputFileName)
+		if create != nil {
+			fmt.Println(line, ":", create)
+			continue
+		}
+		defer outputFile.Close()
 
-  outputFile.Write(body)
+		outputFile.Write(body)
 
-  fmt.Println(line, ":", "Результат сохранен в файл", outputFileName)
- }
+		fmt.Println(line, ":", "Результат сохранен в файл", outputFileName)
+	}
 }
